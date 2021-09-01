d77e518fbaff179233648ddc4a07fe30975cf0ce
ANNAmiuz <yingyihuang2000@outlook.co>
2021-09-01 23:43:06 +0800

    20210901

---
+++ concurrent/getsProc1233.c
@@ -0,0 +1,18 @@
+#include "csapp.h"
+
+char *tfgets(char *s, int size, FILE *stream)
+{
+    
+}
+
+int main(void)
+{
+	char buf[MAXLINE];
+	
+	if (tfgets(buf, MAXLINE, stdin) == NULL)
+		printf("BOOM!\n");
+	else
+		printf("%s", buf);
+	
+	exit(0);
+}
